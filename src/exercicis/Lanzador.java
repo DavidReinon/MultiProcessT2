33a9d44c47f26b66d9b57ee945b9eac77e6c285c
DavidReinon <davidreinongarcia@gmail.com>
2023-10-21 18:03:16 +0200

    Clases sumador y lanzador corregidas

--- src/exercicis/Lanzador.java
+++ src/exercicis/Lanzador.java
@@ -1,75 +1,40 @@
 package exercicis;
 
 import java.io.File;
-
 import java.util.ArrayList;
-
 import java.util.List;
 
 public class Lanzador {
 
-public static void lanzarSumador(Integern1, Integern2){
+	public static void lanzarSumador(Integer n1, Integer n2) {
+		try {
+			String clase = "es.florida.multiproceso.Sumador";
+			String javaHome = System.getProperty("java.home");
+			String javaBin = javaHome + File.separator + "bin" + File.separator + "java";
+			String classpath = System.getProperty("java.class.path");
+			String className = clase;
+			List<String> command = new ArrayList<>();
+			command.add(javaBin);
+			command.add("-cp");
+			command.add(classpath);
+			command.add(className);
+			command.add(n1.toString());
+			command.add(n2.toString());
+			System.out.println("Comando que se pasa a ProcessBuilder: " + command);
+			System.out.println("Comando a ejecutar en cmd.exe: " + command.toString().replace(",", ""));
+			ProcessBuilder builder = new ProcessBuilder(command);
+			Process process = builder.start();
+			process.waitFor();
+			System.out.println(process.exitValue());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-try{
-
-String clase="es.florida.multiproceso.Sumador";
-
-StringjavaHome=System.getProperty("java.home");
-
-StringjavaBin=javaHome+File.separator+"bin"+File.separator+"java";
-
-Stringclasspath=System.getProperty("java.class.path");
-
-StringclassName=clase;
-
-List<String>command=newArrayList<>();
-
-command.add(javaBin);
-
-command.add("-cp");
-
-command.add(classpath);
-
-command.add(className);
-
-command.add(n1.toString());
-
-command.add(n2.toString());
-
-System.out.println("Comando que se pasa a ProcessBuilder: "+command);
-
-System.out.println("Comando a ejecutar en cmd.exe: "+command.toString().replace(",",""));
-
-ProcessBuilderbuilder=newProcessBuilder(command);
-
-Processprocess=builder.start();
-
-// Process process = builder.inheritIO().start();
-
-// Process process = builder.redirectOutput(new File("temp.txt")).start();
-
-process.waitFor();
-
-System.out.println(process.exitValue());
-
-}catch(Exceptione){
-
-e.printStackTrace();
-
-}
-
-}
-
-publicstaticvoidmain(String[]args){
-
-Lanzadorl=newLanzador();
-
-lanzarSumador(1,50);
-
-lanzarSumador(51,100);
-
-System.out.println("Ok");
-
-}
-
+	public static void main(String[] args) {
+		Lanzador l = new Lanzador();
+		lanzarSumador(1, 50);
+		lanzarSumador(51, 100);
+		System.out.println("Ok");
+	}
 }
--- src/exercicis/Sumador.java
+++ src/exercicis/Sumador.java
@@ -1,35 +1,23 @@
 package exercicis;
 
-publicclassSumador{
+public class Sumador {
 
-publicintsumar(intn1,intn2){
+	public int sumar(int n1, int n2) {
+		System.out.println("Sumando de " + n1 + " hasta " + n2);
+		int resultado = 0;
 
-System.out.println("Sumando de "+n1+" hasta "+n2);
+		for (int i = n1; i <= n2; i++) {
+			resultado = resultado + i;
+		}
 
-intresultado=0;
+		return resultado;
+	}
 
-for(inti=n1;i<=n2;i++){
-
-resultado=resultado+i;
-
-}
-
-returnresultado;
-
-}
-
-publicstaticvoidmain(String[]args){
-
-Sumadors=newSumador();
-
-intn1=Integer.parseInt(args[0]);
-
-intn2=Integer.parseInt(args[1]);
-
-intresultado=s.sumar(n1,n2);
-
-System.out.println("Resultado de este sumador: "+resultado);
-
-}
-
+	public static void main(String[] args) {
+		Sumador s = new Sumador();
+		int n1 = Integer.parseInt(args[0]);
+		int n2 = Integer.parseInt(args[1]);
+		int resultado = s.sumar(n1, n2);
+		System.out.println("Resultado de este sumador: " + resultado);
+	}
 }
