4ebe438df235b11cff1417deec8ca55c6709bb78
DavidReinon <davidreinongarcia@gmail.com>
2023-11-20 16:53:20 +0100

    merge desde casa

--- src/exercicis/MiniProjecte8.java
+++ src/exercicis/MiniProjecte8.java
@@ -1,4 +1,5 @@
 package exercicis;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -7,43 +8,44 @@ import java.io.IOException;
 public class MiniProjecte8 {
 
 	public static void main(String[] args) {
-        String inputFile = "datos_neo.txt";
-        String outputDirectory = "resultados_neo/";
+		String inputFile = "datos_neo.txt";
+		String outputDirectory = "resultados_neo/";
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile)) {
-            String line = " ";
-            while ((line = reader.readLine()) != null) {
-                String[] parts = line.split(",");
-                if (parts.length != 3) {
-                    System.err.println("Error: Datos incorrectos en la línea: " + line);
-                    continue;
-                }
+		try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
+			String line = " ";
+			while ((line = reader.readLine()) != null) {
+				String[] parts = line.split(",");
+				if (parts.length != 3) {
+					System.err.println("Error: Datos incorrectos en la línea: " + line);
+					continue;
+				}
 
-                String neoName = parts[0];
-                double positionNEO = Double.parseDouble(parts[1]);
-                double velocityNEO = Double.parseDouble(parts[2]);
+				String neoName = parts[0];
+				double positionNEO = Double.parseDouble(parts[1]);
+				double velocityNEO = Double.parseDouble(parts[2]);
 
-                // Realiza cálculos para determinar la probabilidad de colisión
-                double collisionProbability = calcularProbabilidadColision(positionNEO, velocityNEO);
+				// Realiza cálculos para determinar la probabilidad de colisión
+				double collisionProbability = calcularProbabilidadColision(positionNEO, velocityNEO);
 
-                // Guarda la probabilidad en un archivo independiente
-                String outputFileName = outputDirectory + neoName + ".txt";
-                guardarProbabilidadEnArchivo(outputFileName, collisionProbability);
+				// Guarda la probabilidad en un archivo independiente
+				String outputFileName = outputDirectory + neoName + ".txt";
+				guardarProbabilidadEnArchivo(outputFileName, collisionProbability);
 
-                // Muestra las probabilidades de colisión
-                System.out.println("Probabilidad de colisión para " + neoName + ": " + String.format("%.2f", collisionProbability));
+				// Muestra las probabilidades de colisión
+				System.out.println("Probabilidad de colisión para " + neoName + ": "
+						+ String.format("%.2f", collisionProbability));
 
-                // Genera alertas si es necesario
-                if (collisionProbability > 0.10) {
-                    System.err.println("¡Alerta mundial! Probabilidad de colisión alta para " + neoName);
-                } else {
-                    System.out.println("Todo tranquilo para " + neoName);
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+				// Genera alertas si es necesario
+				if (collisionProbability > 0.10) {
+					System.err.println("¡Alerta mundial! Probabilidad de colisión alta para " + neoName);
+				} else {
+					System.out.println("Todo tranquilo para " + neoName);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	private static double calcularProbabilidadColision(double positionNEO, double velocityNEO) {
 		// Realiza cálculos para determinar la probabilidad de colisión basada en la
